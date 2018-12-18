c6050a679b4a3a3e20577a7365dbd0573e854ff5
Aleksey <carelcontrols@ukr.net>
2018-12-18 14:06:33 +0200

    1

--- src/mate/academy/Main.java
+++ src/mate/academy/Main.java
@@ -12,29 +12,50 @@ public class Main {
 
 		Hero[] fighting = { tadpol, tadpol1 };
 
-		boolean gameOver = false;
+		boolean gameOver = true;
 
-		do {
+		while (gameOver) {
 
 			for (int i = 0; i < 2; i++) {
 
 				switch (i) {
 
 				case 0:
-					System.out.println("Attacking hero " + fighting[i].getName() + " - Stricke is " + fighting[i].strikeHero());
+					
+					if(fighting[i + 1].getHealth() <= 0 || fighting[i].getHealth() <= 0) {
+						gameOver = false;
+						break;
+					}
+					//gameOver = fighting[i + 1].getHealth() <= 0 ? false : true;
+					System.out.println(
+							"Attacking hero " + fighting[i].getName() + " - Stricke is " + fighting[i].strikeHero());
 					fighting[i + 1].setHealth(fighting[i + 1].getHealth() - fighting[i].strikeHero());
-					System.out.println("Health of hero " + fighting[i + 1].getName() + " is " + fighting[i + 1].getHealth());
+
+					System.out.println(
+							"Health of hero " + fighting[i + 1].getName() + " is " + fighting[i + 1].getHealth());
+				
 					break;
 
 				case 1:
-					System.out.println("Attacking hero " + fighting[i].getName() + " - Stricke is " + fighting[i].strikeHero());
+					if(fighting[i - 1].getHealth() <= 0 || fighting[i].getHealth() <= 0) {
+						gameOver = false;
+						break;
+					}
+					
+					//gameOver = fighting[i - 1].getHealth() <= 0 ? false : true;
+					System.out.println(
+							
+							"Attacking hero " + fighting[i].getName() + " - Stricke is " + fighting[i].strikeHero());
 					fighting[i - 1].setHealth(fighting[i - 1].getHealth() - fighting[i].strikeHero());
-					System.out.println("Health of hero " + fighting[i - 1].getName() + " is " + fighting[i - 1].getHealth());
+
+					System.out.println(
+							"Health of hero " + fighting[i - 1].getName() + " is " + fighting[i - 1].getHealth());
+
 					break;
 				}
 
 			}
 
-		} while (gameOver);
+		}
 	}
 }
